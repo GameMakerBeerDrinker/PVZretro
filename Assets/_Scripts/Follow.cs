a73e34780abb9f41580e8fa59a2d951198121b18
ICE-Wol <1192016123@qq.com>
2023-06-23 20:08:18 +0800

    Re again

---
+++ Assets/_Scripts/Follow.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Follow : MonoBehaviour {
+    public Transform parent;
+    public float speed;
+
+    public float curX;
+    public Vector3 curRot;
+
+    public bool rotFollow;
+    public bool ySwing;
+
+    public float oriY;
+    private void FixedUpdate() {
+        curX.ApproachRef(parent.transform.position.x, speed);
+        curRot.ApproachRef(parent.transform.rotation.eulerAngles, speed * Vector3.one);
+
+        if (ySwing) transform.position = new Vector3(curX, oriY + 0.1f * Mathf.Sin(Time.time * 5f), 0f);
+        else transform.position = new Vector3(curX, transform.position.y, 0f);
+        if(rotFollow) transform.rotation = Quaternion.Euler(curRot);
+    }
+}
---
+++ Assets/_Scripts/ZombieAnim.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ZombieAnim : MonoBehaviour {
+
+    public int timer;
+    public int nextMoveTime;
+    public float nextXPos;
+    public float curXPos;
+    public Vector3 nextRandomRot;
+    public Vector3 curRot;
+    public Transform head;
+    public Transform bloodPrefab;
+    public Transform[] bloodCircle;
+
+    public int hp;
+
+    private void Start() {
+        bloodCircle = new Transform[hp];
+        for (int i = 0; i < 10; i++) {
+            bloodCircle[i] = Instantiate(bloodPrefab, transform);
+            bloodCircle[i].transform.localPosition = 1f * Calc.Deg2Dir3(i * 36f);
+        }
+    }
+
+    public void RefreshBlood() {
+        for (int i = 0; i < hp; i++) {
+            bloodCircle[i].transform.position = transform.position + 1f * Calc.Deg2Dir3(i * 36f + 2f * timer);
+        }
+
+        for (int i = hp; i < 10; i++) {
+            bloodCircle[i].transform.position = transform.position;
+        }
+    }
+
+
+
+    private void Update() {
+        if(Input.anyKeyDown) hp--;
+    }
+    private void FixedUpdate() {
+        RefreshBlood();
+        Movement();
+        timer++;
+    }
+
+    private void Movement() {
+        if (timer == nextMoveTime) {
+            nextXPos -= Random.Range(0.8f, 1.2f);
+            nextMoveTime += Random.Range(100, 150);
+            nextRandomRot += new Vector3(Random.Range(-90f, 90f), Random.Range(-90f, 90f), Random.Range(-90f, 90f));
+        }
+
+        curRot.ApproachRef(nextRandomRot, 32f * Vector3.one);
+        curXPos.ApproachRef(nextXPos, 64f);
+        
+        head.transform.rotation = Quaternion.Euler(curRot);
+        head.transform.position = new Vector3(curXPos, head.transform.position.y, 0f);
+    }
+}
